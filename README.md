53e1f5983c791484a722f393a2f9a21693e205ae
l-gorman <lgorman@turing.ac.uk>
2022-01-27 11:38:29 +0000

    updating guidelines and ignoring outputs

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
-# Getting Started
+# RHoMIS 2.0 Local Processing Scripts
+
+This is a suggested setup for processing RHoMIS data sets locally, using the [RHoMIS R-package](https://github.com/l-gorman/rhomis-R-package).
+
+## Getting the Code
+### Using the Command Line
 
 This is a folder showing how to process projects locally
-using the RHoMIS R-package. All of the outputs in the folder
-can be reproduced using only:
+using the RHoMIS R-package. If you are familiar with using the command line, and have git installed, you can clone and rename the repository using the command:
 
-* Raw_Data.csv
-* extractValues.R
-* processData.R
+`git clone https://github.com/l-gorman/rhomis-local-processing-scripts.git PROJECT_NAME`
 
-The remaining folders are all outputs from the R-package.
+*Replace `PROJECT_NAME` with the project name you would like to use.*
 
-The file `usingRHoMIS.Rmd` is an Rmarkdown file with guidance
-on how I have processed the data, and how you could reproduce 
-the results yourself. This is just a first run at it so it would
-be great to hear your thoughts on it. If you only want to read
-the output (which I recommend), I would open the file "usingRHoMIS.html"
+### Manual Download
+
+Alternatively, download the whole repository by selecting the drop-down menu labelled 'code', then selecting the 'Download ZIP' option. Once you have downloaded the zip, extract the files into a folder where you would like to conduct the analysis. It can be helpful to give this folder a meaningful name (e.g. a project name)
+
+## Processing Datasets
+
+The file `usingRHoMIS.Rmd` contains guidance
+on how I have processed the data. This is still under development so it would
+be great to hear your thoughts on it. You can open this documentation in your browser by opening the file `usingRHoMIS.html`
 file. This should open directly in your browser. If it does not, then
 simply right-click and manually select open in browser.
 
+## Suggesting Changes
+
+Please report any problems you encounter in the issues section of this repo. 
--- valueCalculations.R
+++ valueCalculations.R
@@ -1,14 +1,14 @@
 # usr/bin/env Rscript
 
 #' Value calculations
-#' 
-#' This is a test Rscript for those using the RHoMIS 
-#' R package locally. This demonstrates how locally 
-#' generated outputs can be used to calculate the 
+#'
+#' This is a test Rscript for those using the RHoMIS
+#' R package locally. This demonstrates how locally
+#' generated outputs can be used to calculate the
 #' values of products consumed.
-#' 
+#'
 #' If you need to install or update the RHoMIS package, enter the command (please ensure you have the "devtools" package installed):
-#' 
+#'
 #' devtools::install_github("git@github.com:l-gorman/rhomis-R-package.git")
 
 library(rhomis)
@@ -16,51 +16,55 @@ library(readr)
 
 list.files("crop_data/")
 
-indicators <-  readr::read_csv("./indicators/indicators.csv", 
-                               col_types = readr::cols())
+indicators <- readr::read_csv("./indicators/indicators.csv",
+  col_types = readr::cols()
+)
 
 
 # Value crop consumed
 
-crop_consumed <- readr::read_csv("./crop_data/crop_consumed_kg_per_year.csv", 
-                                 col_types = readr::cols())
+crop_consumed <- readr::read_csv("./crop_data/crop_consumed_kg_per_year.csv",
+  col_types = readr::cols()
+)
 
 mean_crop_prices <- readr::read_csv("./mean_prices/crop_prices.csv",
-                                    col_types = readr::cols(), na = "Inf")
+  col_types = readr::cols(), na = "Inf"
+)
 
 # check that the orders of the data frames are the same
-if (ncol(crop_consumed)!=ncol(mean_crop_prices)){
+if (ncol(crop_consumed) != ncol(mean_crop_prices)) {
   stop("Different numbers of columns in crop consumed and crop prices")
 }
 # Check all of the columns are present in both datasets
-if (all(colnames(crop_consumed)%in%colnames(mean_crop_prices))==F |
-    all(colnames(mean_crop_prices)%in%colnames(crop_consumed))==F){
+if (all(colnames(crop_consumed) %in% colnames(mean_crop_prices)) == F |
+  all(colnames(mean_crop_prices) %in% colnames(crop_consumed)) == F) {
   stop("Different column names crop consumed and crop prices")
 }
 # Check all of the columns are the same order in both datasets
-if (all(colnames(crop_consumed)==colnames(mean_crop_prices))==F |
-    all(colnames(mean_crop_prices)==colnames(crop_consumed))==F){
+if (all(colnames(crop_consumed) == colnames(mean_crop_prices)) == F |
+  all(colnames(mean_crop_prices) == colnames(crop_consumed)) == F) {
   stop("Different column names crop consumed and crop prices")
 }
 
-value_crop_consumed <- crop_consumed*as.numeric(mean_crop_prices[1,])/indicators$conversion_factor
+value_crop_consumed <- crop_consumed * as.numeric(mean_crop_prices[1, ]) / indicators$conversion_factor
 
 
 # Value Egg Consumed
-egg_consumed <- readr::read_csv("./livestock_data/eggs_consumed_kg_per_year.csv", 
-                                 col_types = readr::cols())
+egg_consumed <- readr::read_csv("./livestock_data/eggs_consumed_kg_per_year.csv",
+  col_types = readr::cols()
+)
 
 mean_egg_prices <- readr::read_csv("./mean_prices/egg_price_per_kg.csv",
-                                    col_types = readr::cols(), na = "Inf")
+  col_types = readr::cols(), na = "Inf"
+)
 
-value_egg_consumed <- egg_consumed*as.numeric(mean_egg_prices[1,])/indicators$conversion_factor
+value_egg_consumed <- egg_consumed * as.numeric(mean_egg_prices[1, ]) / indicators$conversion_factor
 
 
 # Alternatively, you could use the actual reported prices
 
 egg_prices_all <- readr::read_csv("./livestock_data/eggs_price_per_kg.csv",
-                                   col_types = readr::cols())
+  col_types = readr::cols()
+)
 
-value_egg_consumed <- egg_consumed*egg_prices_all/indicators$conversion_factor
-
-
+value_egg_consumed <- egg_consumed * egg_prices_all / indicators$conversion_factor
